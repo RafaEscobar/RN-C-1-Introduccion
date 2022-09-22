58ec872cd2905e92c56f06e9e163a43b8eb81162
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-09-22 15:50:07 -0500

    Funciones -Codigo 75%-

---
+++ src/typescript/Funciones.tsx
@@ -0,0 +1,13 @@
+
+
+export const Funciones = () => {
+    const suma = (a:number,b:number):number =>{
+        return (200/10);
+    }
+  return (
+    <>
+        <h3>Funciones</h3>
+        <span>El resultado de no se que cosa es: {suma(10,2)}</span>
+    </>
+  )
+}
