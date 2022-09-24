ef8e1af551398195bc06dbb5b6cd61d0f5986451
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-09-24 14:48:26 -0500

    Practica de -PETICIONES HTTP- al 50%

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,9 +1,36 @@
+import { useEffect } from "react"
+import { reqResApi } from '../api/reqRes';
 
 
 export const Usuarios = () => {
+
+    useEffect(() => {
+      // Llamado a la API
+        reqResApi.get('/users')
+            .then( resp => {
+                console.log( resp.data );
+            })
+            .catch( err => console.log( err ) );
+    }, [])
+    
+
   return (
     <>
         <h3>Usuarios</h3>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Identificador</th>
+                    <th>Nombre</th>
+                    <th>Correo electronico</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
     </>
   )
 }
