d82979a8bf9f92ed142dddaaf88fd1679bfa4c74
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-09-24 17:33:34 -0500

    Practica de -PETICIONES HTTP- 65%

---
+++ src/interfaces/resRes.tsx
@@ -0,0 +1,27 @@
+
+export interface ReqResInterface {
+    page:        number;
+    per_page:    number;
+    total:       number;
+    total_pages: number;
+    /* 
+        Es recomendable cambiar 'Dump[]' que es lo que nos arroja el typado automatico
+        por aquello que vamos a obtener, en este caso, una lista de usuarios, quedando
+        como:: Usuario[]
+    */
+    data:        Usuario[];
+    support:     Support;
+}
+
+export interface Usuario {
+    id:         number;
+    email:      string;
+    first_name: string;
+    last_name:  string;
+    avatar:     string;
+}
+
+export interface Support {
+    url:  string;
+    text: string;
+}
